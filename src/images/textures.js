8cb46666c7bf56ff2711b669d9dfa6bf399ae40e
Ilya <n2i1a95@yandex.ru>
2023-08-17 16:38:35 +0300

    textureLoader in separate texture.js file

---
+++ src/images/textures.js
@@ -0,0 +1,16 @@
+import { TextureLoader } from 'three'
+
+import  {
+	dirtImg,
+	grassImg,
+	glassImg,
+	woodImg,
+	logImg,
+} from './images'
+
+const dirtTexture = new TextureLoader(dirtImg)
+const logTexture = new TextureLoader(logImg)
+const grassTexture = new TextureLoader(grassImg)
+const woodTexture = new TextureLoader(woodImg)
+const glassTexture = new TextureLoader(glassImg)
+const groundTexture = new TextureLoader(grassImg)
