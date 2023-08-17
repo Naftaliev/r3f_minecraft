c6b576d32eda11a07d55b4572d4bb453ccd5e5a0
Ilya <n2i1a95@yandex.ru>
2023-08-17 18:12:28 +0300

    added Player

---
+++ src/components/Player.js
@@ -0,0 +1,17 @@
+import { useThree } from "@react-three/fiber"
+import { useSphere } from "@react-three/cannon"
+
+export const Player = () => {
+    const {camera} = useThree()
+    const [ref] = useSphere(() => ({
+        mass: 1,
+        type: 'Dynamic',
+        position: [0, 0, 0]
+
+    }))
+
+    return (
+        <mesh ref={ref}>
+        </mesh>
+    )
+}
