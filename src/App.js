619afb2e82b1ad58d185420f7ecfca788a8aee3f
Ilya <n2i1a95@yandex.ru>
2023-08-19 09:18:11 +0300

    mouse controls from drei and cursor in the center of the screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
 import { Player } from './components/Player';
+import { FPV } from './components/FPV';
 
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
         <Sky sunPosition = {100, 100, 30} />
         <ambientLight intensity= {0.5} />
         <Physics>
+          <FPV />
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className='absolute centered  cursor'>+</div>
     </>
   );
 }
